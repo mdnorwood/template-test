d7376e027ef0a57bb0527259d04f7f1bec17379f
Marcus Norwood <marcusnorwood@Marcuss-Air.hsd1.co.comcast.net>
2021-02-05 08:17:16 -0700

    Commit quote generator build

---
+++ script-2.js
@@ -0,0 +1,75 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// let apiQuotes = [];
+
+
+
+// Show New Quote
+function newQuote() {
+    showLoadingSpinner();
+    // Pick a random quote form apiQuotes Array
+    // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // Check if author field is blank and place it with 'Unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+
+
+// Get Quotes From API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes/';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
+
+//Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On Load
+// getQuotes();
+newQuote();
